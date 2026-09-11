d8d5c781066d4c42d554ffeadb5af6c88b6b4a5d
hirobuminagao <257726042+hirobuminagao@users.noreply.github.com>
2026-09-11 16:19:29 +0900

    Make subscriber HIA index migration idempotent

--- sql/migrations/dev_phr/20260911_001_dev_phr_add_subscriber_hia_index.sql
+++ sql/migrations/dev_phr/20260911_001_dev_phr_add_subscriber_hia_index.sql
@@ -1,2 +1,15 @@
-ALTER TABLE `dev_phr`.`subscribers`
-  ADD KEY `idx_subscribers_hia_subscriber_id` (`hia_subscriber_id`);
+SET @add_subscribers_hia_index_sql = (
+  SELECT IF(
+    COUNT(*) = 0,
+    'ALTER TABLE `dev_phr`.`subscribers` ADD KEY `idx_subscribers_hia_subscriber_id` (`hia_subscriber_id`)',
+    'SELECT 1'
+  )
+  FROM information_schema.statistics
+  WHERE table_schema = 'dev_phr'
+    AND table_name = 'subscribers'
+    AND index_name = 'idx_subscribers_hia_subscriber_id'
+);
+
+PREPARE add_subscribers_hia_index_stmt FROM @add_subscribers_hia_index_sql;
+EXECUTE add_subscribers_hia_index_stmt;
+DEALLOCATE PREPARE add_subscribers_hia_index_stmt;
